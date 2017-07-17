bb018f0430e95672316d151ca7575e9a64221d1b
adhokshaja <adok.achar@gmail.com>
2017-07-17 08:18:10 -0400

    chore: updated packages ans ignores

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'app.bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Project Demo',
+            // minify: {
+            //     collapseWhitespace: true
+            // },
+            hash: true,
+            template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
+        })
+    ]
+}
